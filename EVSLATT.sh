39e78f0dd3a65a2d0bad2aaac3ecacf5a20ac122
Don Mathew <144115635+donmathew008@users.noreply.github.com>
2024-05-11 22:36:17 +0530

    Update EVSLATT.sh

--- EVSLATT.sh
+++ EVSLATT.sh
@@ -2,7 +2,7 @@
   for i in `seq -w 10.00 1.00 15.00`
  do
 
-  sed -e "s/LATT/$i/g" gold.fdf >\ Au.$i.fdf
+  sed -e "s/LATT/$i/g" gold.fdf >| Au.$i.fdf
  siesta < Au.$i.fdf | tee Au.$i.out
 done
 grep "Total =" *.out | tee energy.dat
